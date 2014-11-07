dc0d7db98d3764de1de87d65b6f2a633c4404e1e
MaoShaco <maoshaco@gmail.com>
2014-11-08 01:56:33 +0300

    SpriteObject
    The class which will help to represent a content like a name of content
    parameter and then retrieves the content from contentManager

---
+++ Equipment/SpriteGameObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class SpriteGameObject : GameObject
+{
+    #region Fields
+    public SpriteSheet Sprite { get; protected set; }
+    public Vector2 Origin { get; set; }
+    #endregion
+
+    #region Constructor
+    public SpriteGameObject(string contentName, int Layer = 0, string ID = "", int sheetIndex = 0)
+        : base(Layer, ID)
+    {
+        if (contentName != "")
+            Sprite = new SpriteSheet(contentName, sheetIndex);
+        else
+            Sprite = null;
+    }
+    #endregion
+
+    #region Methods
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        Sprite.Draw(spriteBatch, this.GlobalPosition, Origin);
+    }
+
+    public Rectangle BoundingBox
+    {
+        get
+        {
+            int left = (int)(GlobalPosition.X - Origin.X);
+            int top = (int)(GlobalPosition.Y - Origin.Y);
+            return new Rectangle(left, top, Sprite.SheetWidth, Sprite.SheetHeight);
+        }
+    }
+    #endregion
+}
+
