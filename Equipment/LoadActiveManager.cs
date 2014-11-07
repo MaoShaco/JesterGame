c167c9d4216bfd71862bf513b67f84753e518061
MaoShaco <maoshaco@gmail.com>
2014-11-07 11:01:31 +0300

    LoadManager
    Just a manager which help us to load a content in every special case

---
+++ Equipment/LoadActiveManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+public class LoadActiveManager
+{
+    #region Fields
+    public ContentManager ContentManager { get; private set; }
+    #endregion
+
+    #region Constructor
+    public LoadActiveManager(ContentManager Content)
+    {
+        this.ContentManager = Content;
+    }
+    #endregion
+
+    #region Methods
+    public Texture2D GetSprite(string contentName)
+    {
+        return ContentManager.Load<Texture2D>(contentName);
+    }
+
+    public void PlaySound(string contentName)
+    {
+        SoundEffect snd = ContentManager.Load<SoundEffect>(contentName);
+        snd.Play();
+    }
+
+    public void PlayMusic(string contentName, bool repeat = true)
+    {
+        MediaPlayer.IsRepeating = repeat;
+        MediaPlayer.Play(ContentManager.Load<Song>(contentName));
+    }
+    #endregion
+}
