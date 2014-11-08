10200620e389bbbb4b55642f198316d693c5bee4
MaoShaco <maoshaco@gmail.com>
2014-11-08 11:08:45 +0300

    TextObject
    Class which represents the text in game using special spriteFonts

---
+++ Equipment/TextGameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class TextGameObject : GameObject
+{
+    #region Fields
+    private SpriteFont spriteFont;
+
+    public Color Color { get; set; }
+    public string Text { get; set; }
+    #endregion
+
+    #region Constructor
+    public TextGameObject(string contentName, int Layer = 0, string ID = "")
+        : base(Layer, ID)
+    {
+        spriteFont = GameEnvironment.LoadManager.ContentManager.Load<SpriteFont>(contentName);
+        Color = Color.White;
+    }
+    #endregion
+
+    #region
+    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+    {
+        spriteBatch.DrawString(spriteFont, Text, this.GlobalPosition, Color);
+    }
+    #endregion
+}
+
