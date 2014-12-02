84fb12d717ed9978139fc9ad6aacd6b62d317725
MaoShaco <maoshaco@gmail.com>
2014-12-02 16:27:01 +0300

    Buttons in game
    Special button which will represent inGame buttons, Quit, Back, so on...

---
+++ InGameClasses/Button.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+class Button : SpriteGameObject
+{
+    #region Fields
+
+    public bool Pressed { get; private set; }
+
+    #endregion
+
+    #region Constructor
+
+    public Button(string imageAsset, int layer = 0, string id = "")
+        : base(imageAsset, layer, id)
+    {
+        Pressed = false;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        Pressed = inputHelper.MouseLeftButtonPressed() && BoundingBox.Contains((int)inputHelper.MousePosition.X, (int)inputHelper.MousePosition.Y);
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+        Pressed = false;
+    }
+    #endregion
+}
