9cb60417c9b6f4de05b81eb4fa2456640f48a640
MaoShaco <maoshaco@gmail.com>
2014-12-09 13:03:13 +0300

    Level&Loading
    Class which load the level and represent all objects from .txt file for
    all InGame Objects

---
+++ InGameClasses/Level.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework;
+
+partial class Level : GameObjectList
+{
+    #region Fields
+
+    public bool Locked { get; set; }
+    public bool Solved { get; set; }
+    private Button QuitButton;
+
+    public bool GameOver
+    {
+        get
+        {
+            Player player = this.Find("player") as Player;
+            return !player.IsAlive;
+        }
+    }
+
+    #endregion
+    #region Constructor
+
+    public Level(int levelIndex)
+    {
+        GameObjectList backgrounds = new GameObjectList(0, "backgrounds");
+        SpriteGameObject background_main = new SpriteGameObject("Backgrounds/spr_sky");
+        background_main.Position = new Vector2(0, GameEnvironment.Screen.Y - background_main.Sprite.SheetHeight);
+        backgrounds.Add(background_main);
+
+        for (int i = 0; i < 5; i++)
+        {
+            SpriteGameObject mountain = new SpriteGameObject("Backgrounds/spr_mountain_" + (GameEnvironment.Random.Next(2) + 1), 1);
+            mountain.Position = new Vector2((float)GameEnvironment.Random.NextDouble() * GameEnvironment.Screen.X - mountain.Sprite.SheetWidth / 2, GameEnvironment.Screen.Y - mountain.Sprite.SheetHeight);
+            backgrounds.Add(mountain);
+        }
+
+        Clouds clouds = new Clouds(2);
+        backgrounds.Add(clouds);
+        this.Add(backgrounds);
+
+        QuitButton = new Button("Sprites/spr_button_quit", 100);
+        QuitButton.Position = new Vector2(GameEnvironment.Screen.X - QuitButton.Sprite.SheetWidth - 10, GameEnvironment.Screen.Y - QuitButton.Sprite.SheetHeight- 10);
+        this.Add(QuitButton);
+
+
+        this.Add(new GameObjectList(1, "ZenyS"));
+        this.Add(new GameObjectList(2, "enemies"));
+
+        this.LoadTiles("Content/Levels/" + levelIndex + ".txt");
+    }
+
+    #endregion
+    #region Methods
+
+    public bool Completed
+    {
+        get
+        {
+            SpriteGameObject exitObj = this.Find("exit") as SpriteGameObject;
+            Player player = this.Find("player") as Player;
+            if (!exitObj.CollidesWith(player))
+                return false;
+            GameObjectList ZenyS = this.Find("ZenyS") as GameObjectList;
+            foreach (GameObject d in ZenyS.gameObjects)
+                if (d.Visible)
+                    return false;
+            return true;
+        }
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        base.HandleInput(inputHelper);
+        if (QuitButton.Pressed)
+        {
+            this.Reset();
+            GameEnvironment.GameStateManager.SwitchTo("levelMenu");
+        }
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        Player player = this.Find("player") as Player;
+
+        if (this.Completed)
+        {
+            player.LevelFinished();
+        }
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+    }
+
+    #endregion
+}
+
---
+++ InGameClasses/LevelLoading.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+
+partial class Level : GameObjectList
+{
+    #region Methods
+
+    public void LoadTiles(string path)
+    {
+        int width;
+        List<string> textlines = new List<string>();
+        StreamReader fileReader = new StreamReader(path);
+        string line = fileReader.ReadLine();
+        width = line.Length;
+        while (line != null)
+        {
+            textlines.Add(line);
+            line = fileReader.ReadLine();
+        }
+        TileField tiles = new TileField(textlines.Count - 1, width, 1, "tiles");
+
+        this.Add(tiles);
+        tiles.CellWidth = 72;
+        tiles.CellHeight = 55;
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < textlines.Count - 1; ++y)
+            {
+                Tile t = LoadTile(textlines[y][x], x, y);
+                tiles.Add(t, x, y);
+            }
+    }
+
+    private Tile LoadTile(char tileType, int x, int y)
+    {
+        switch (tileType)
+        {
+            case '.':
+                return new Tile();
+            case '-':
+                return LoadBasicTile("spr_platform", TileType.Platform);
+            case '@':
+                return LoadBasicTile("spr_platform_ice", TileType.Platform, false, true);
+            case 'X':
+                return LoadEndTile(x, y);
+            case 'W':
+                return LoadZenyTile(x, y);
+            case '1':
+                return LoadStartTile(x, y);
+            case '#':
+                return LoadBasicTile("spr_wall", TileType.Normal);
+            case '*':
+                return LoadBasicTile("spr_wall_ice", TileType.Normal, false, true);
+            default:
+                return new Tile("");
+        }
+    }
+
+    private Tile LoadBasicTile(string name, TileType tileType, bool hot = false, bool ice = false)
+    {
+        Tile t = new Tile("Tiles/" + name, tileType);
+        t.Ice = ice;
+        return t;
+    }
+
+    private Tile LoadStartTile(int x, int y)
+    {
+        TileField tiles = this.Find("tiles") as TileField;
+        Vector2 startPosition = new Vector2(((float)x + 0.5f) * tiles.CellWidth, (y + 1) * tiles.CellHeight);
+        Player player = new Player(startPosition);
+        this.Add(player);
+        return new Tile("", TileType.Background);
+    }
+
+    private Tile LoadEndTile(int x, int y)
+    {
+        TileField tiles = this.Find("tiles") as TileField;
+        SpriteGameObject exitObj = new SpriteGameObject("Sprites/spr_goal", 1, "exit");
+        exitObj.Position = new Vector2(x * tiles.CellWidth, (y + 1) * tiles.CellHeight);
+        exitObj.Origin = new Vector2(0, exitObj.Sprite.SheetHeight);
+        this.Add(exitObj);
+        return new Tile();
+    }
+
+    private Tile LoadZenyTile(int x, int y)
+    {
+        GameObjectList ZenyS = this.Find("ZenyS") as GameObjectList;
+        TileField tiles = this.Find("tiles") as TileField;
+        Zeny w = new Zeny();
+        w.Origin = w.Center;
+        w.Position = new Vector2(x * tiles.CellWidth, y * tiles.CellHeight - 10);
+        w.Position += new Vector2(tiles.CellWidth, tiles.CellHeight) / 2;
+        ZenyS.Add(w);
+        return new Tile();
+    }
+    #endregion
+}
